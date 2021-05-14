a2ba3eaf83f30685027191273c019b9fea2c600f
Muhammad-Jibril <jbashir52@gmail.com>
2021-05-14 10:43:18 +0800

    add colors constant

--- src/constants/networkSpect.ts
+++ src/constants/networkSpect.ts
@@ -1,3 +1,25 @@
+const colors = {
+  background: {
+    app: "#70c1ff", // a shade of "sky blue"
+    button: "#0084ff", // a shade of "pure blue"
+    card: "#63a9ff", // a darker shade of "sky blue"
+    os: "#49006b", // a shade of "dark purple"
+  },
+  border: {
+    dark: "#000000", // "black"
+    light: "#7d7d7d", // a shade of "gray"
+    signal: "#00bfff", // a light shade of "blue"
+  },
+  signal: {
+    error: "#e6192a", // a shade of "red"
+    main: "#47ffea", // a shade of "turquoise"
+  },
+  text: {
+    faded: "#9A9A9A", // a shade of "gray"
+    main: "#C0C0C0", // a brighter shade of "gray"
+  },
+};
+
 export const unknownNetworkPathId = "";
 
 export const NetworkProtocols = Object.freeze({
