0d1eea5560f6e2aa4d1de22147be8a159c5143b3
Muhammad-Jibril <jbashir52@gmail.com>
2021-05-14 09:33:50 +0800

    add unknown networks types

---
+++ src/constants/networkSpect.ts
@@ -0,0 +1,10 @@
+export const unknownNetworkPathId = "";
+
+export const NetworkProtocols = Object.freeze({
+  UNKNOWN: "unknown",
+});
+
+// accounts for which the network couldn't be found (failed migration, removed network)
+export const UnknownNetworkKeys = Object.freeze({
+  UNKNOWN: "unknown",
+});
