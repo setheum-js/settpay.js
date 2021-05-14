f18d535dda37567d16b8254ecb6231503591d156
Muhammad-Jibril <jbashir52@gmail.com>
2021-05-14 10:58:14 +0800

    ADD SUBSTRATE, UNKNOWN AND DEFAULT NETWORK

--- src/constants/networkSpect.ts
+++ src/constants/networkSpect.ts
@@ -24,6 +24,7 @@ export const unknownNetworkPathId = "";
 
 export const NetworkProtocols = Object.freeze({
   ETHEREUM: "ethereum",
+  SUBSTRATE: "substrate",
   UNKNOWN: "unknown",
 });
 
@@ -32,6 +33,8 @@ export const UnknownNetworkKeys = Object.freeze({
   UNKNOWN: "unknown",
 });
 
+// Ethereum --------------------------------------------------------
+//
 // ethereumChainId is used as Network key for Ethereum networks
 /* eslint-disable sort-keys */
 export const EthereumNetworkKeys = Object.freeze({
@@ -84,3 +87,85 @@ const ethereumDefaultValues = {
 export const ETHEREUM_NETWORK_LIST = Object.freeze(
   setDefault(ethereumNetworkBase, ethereumDefaultValues)
 );
+
+// Substrate --------------------------------------------------------
+//
+/* eslint-enable sort-keys */
+// genesisHash is used as Network key for Substrate networks
+export const SubstrateNetworkKeys = Object.freeze({
+  SUBSTRATE_DEV:
+    "0x0d667fd278ec412cd9fccdb066f09ed5b4cfd9c9afa9eb747213acb02b1e70bc", // substrate --dev commit ac6a2a783f0e1f4a814cf2add40275730cd41be1 hosted on wss://dev-node.substrate.dev .
+});
+
+
+const substrateNetworkBase = {
+  [SubstrateNetworkKeys.SUBSTRATE_DEV]: {
+    color: "#18FFB2",
+    decimals: 12,
+    genesisHash: SubstrateNetworkKeys.SUBSTRATE_DEV,
+    order: 100,
+    pathId: "substrate_dev",
+    prefix: 42,
+    title: "Substrate Development",
+    unit: "UNIT",
+  }
+};
+
+// Unknown Networks ----------------------------------------------------
+//
+const unknownNetworkBase = {
+  [UnknownNetworkKeys.UNKNOWN]: {
+    color: colors.signal.error,
+    order: 99,
+    pathId: unknownNetworkPathId,
+    prefix: 2,
+    protocol: NetworkProtocols.UNKNOWN,
+    secondaryColor: colors.background.card,
+    title: "Unknown network",
+  },
+};
+
+const substrateDefaultValues = {
+  color: "#4C4646",
+  protocol: NetworkProtocols.SUBSTRATE,
+  secondaryColor: colors.background.card,
+};
+
+export const SUBSTRATE_NETWORK_LIST = Object.freeze(
+  setDefault(substrateNetworkBase, substrateDefaultValues)
+);
+export const UNKNOWN_NETWORK = Object.freeze(unknownNetworkBase);
+
+const substrateNetworkMetas = Object.values({
+  ...SUBSTRATE_NETWORK_LIST,
+  ...UNKNOWN_NETWORK,
+});
+export const PATH_IDS_LIST = substrateNetworkMetas.map(
+  (meta: any) => meta.pathId
+);
+
+// networks list
+export const NETWORK_LIST = Object.freeze(
+  Object.assign(
+    {},
+    SUBSTRATE_NETWORK_LIST,
+    ETHEREUM_NETWORK_LIST,
+    UNKNOWN_NETWORK
+  )
+);
+
+// set default network base
+function setDefault(networkBase, defaultProps) {
+  return Object.keys(networkBase).reduce((acc, networkKey) => {
+    return {
+      ...acc,
+      [networkKey]: {
+        ...defaultProps,
+        ...networkBase[networkKey],
+      },
+    };
+  }, {});
+}
+
+// the default network
+export const defaultNetworkKey = SubstrateNetworkKeys.SUBSTRATE_DEV;
